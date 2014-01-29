fc113e4609cec42202b5565b07e7d8ee0a3b179e
Martin Smith <martin@mbs3.org>
2014-01-29 12:44:00 -0600

    working on exercise 10, wrapper cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,16 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+include_recipe "rackspace_memcached"
+
+case node['memcached']['config']['iptables']
+when "open"
+  port = node['memcached']['config']['port']
+  chain = 'INPUT'
+  rule = "-p tcp --dport #{port} -j ACCEPT"
+  node.set['rackspace_iptables']['config']['chains'][chain][rule]['weight'] = 50
+  node.set['rackspace_iptables']['config']['chains'][chain][rule]['comment'] = "open memcached port"
+end
+
+include_recipe "rackspace_iptables"
